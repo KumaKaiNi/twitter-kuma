f5d40eb3f701884d18de2f3bcfc4ba3e9af5c670
rekyuu <senkan.re.kyuu@gmail.com>
2017-08-29 22:29:46 -0400

    Initial commit

---
+++ lib/twitter_kuma.ex
@@ -0,0 +1,18 @@
+defmodule TwitterKuma do
+  @moduledoc """
+  Documentation for TwitterKuma.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TwitterKuma.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/twitter_kuma_test.exs
@@ -0,0 +1,8 @@
+defmodule TwitterKumaTest do
+  use ExUnit.Case
+  doctest TwitterKuma
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
