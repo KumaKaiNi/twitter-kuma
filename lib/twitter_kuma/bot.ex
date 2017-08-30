df824ce5121d38a48583e8d0df7578192fcc6e07
rekyuu <senkan.re.kyuu@gmail.com>
2017-08-29 20:51:35 -0600

    starting supervisor

--- lib/twitter_kuma/bot.ex
+++ lib/twitter_kuma/bot.ex
@@ -1,9 +1,8 @@
 defmodule TwitterKuma.Bot do
   use GenServer
+  require Logger
 
   def start_link(opts \\ []) do
-    require Logger
-
     Logger.debug "Starting bot!"
     GenServer.start_link(__MODULE__, :ok, opts)
   end
@@ -14,7 +13,9 @@ defmodule TwitterKuma.Bot do
   end
 
   def handle_info(:update, state) do
-    ExTwitter.update(TwitterKuma.Markov.get_markov)
+    update = TwitterKuma.Markov.get_markov
+    Logger.debug "Sending update: #{update}"
+    ExTwitter.update update
 
     :erlang.send_after(3_600_000, self, :update)
     {:noreply, state}
--- mix.exs
+++ mix.exs
@@ -15,7 +15,8 @@ defmodule TwitterKuma.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger, :extwitter]]
+    [extra_applications: [:logger, :extwitter],
+     mod: {TwitterKuma, []}]
   end
 
   # Dependencies can be Hex packages:
